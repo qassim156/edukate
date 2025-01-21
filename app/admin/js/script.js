618e72902efc294f35b4c5dd4a7d4c206e8987f7
qassim156 <59389885+qassim156@users.noreply.github.com>
2025-01-21 09:17:53 +0100

    Updating Admin Pages

--- app/admin/js/script.js
+++ app/admin/js/script.js
@@ -1,1 +1,33 @@
-// 
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 5
+    }]
+  },
+  options: {
+    
+  }
+});
+
+const ctx2 = document.getElementById('myChart-1');
+
+new Chart(ctx2, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 5
+    }]
+  },
+  options: {
+    
+  }
+});
