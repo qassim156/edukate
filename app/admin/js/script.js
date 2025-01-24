25365cbb0bd31413498d4adfff3c618b07a3b2fa
qassim156 <59389885+qassim156@users.noreply.github.com>
2025-01-24 16:15:55 +0100

    html updates

--- app/admin/js/script.js
+++ app/admin/js/script.js
@@ -1,17 +1,26 @@
 const ctx = document.getElementById('myChart');
 
 new Chart(ctx, {
-  type: 'doughnut',
+  type: 'pie',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Male Students', 'Female Students'],
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 5
+      label: 'Number in Total',
+      data: [1900, 909],
+      borderWidth: 2,
+      backgroundColor: [
+        'rgba(0, 105, 217, 0.39)',
+        'rgba(0, 157, 255, 0.32)'
+      ],
     }]
   },
   options: {
-    
+    scales: {
+      y: {
+        data: [100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000],
+        beginAtZero: true 
+      }
+    }
   }
 });
 
